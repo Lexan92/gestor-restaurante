9a5c7d274a3dadf7a1253ebde9796a9696b8b4fd
Luis Avendaño <luis_grennby92@hotmail.com>
2025-06-24 21:08:26 -0600

    Fix extends annotation case

--- database/factories/CategoriaProductosFactory.php
+++ database/factories/CategoriaProductosFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\categoriaProductos>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CategoriaProductos>
  */
 class CategoriaProductosFactory extends Factory
 {
